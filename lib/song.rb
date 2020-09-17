7faf2fdf38ac2550735e151baa80cab9a1386977
Jessica Watts <jessperr182@hotmail.com>
2020-09-17 19:13:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,30 @@
-class Song
-  attr_accessor :name, :artist_name
-  @@all = []
+class Artist
+  attr_accessor :name
 
-  def self.all
-    @@all
-  end
+  @@song_count = 0
 
-  def save
-    self.class.all << self
-  end
-  
   def initialize(name)
-    @name = name 
-  end 
-  
-   Song.create_by_name
-end
+    @name = name
+    @songs = []
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song_name)
+    @songs << song_name
+    song_name.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
